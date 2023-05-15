1348ba1d581ac9f4b1284efa73484cc213a61e14
habibur <va.habibur@gmail.com>
2023-05-15 13:27:52 +0600

    implemented pagination

--- index.js
+++ index.js
@@ -28,10 +28,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const productCollection = client.db('emaJhonSimpleDB').collection('products')
 
         // OPERATIONS 
+        // get all products 
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page) || 0
+            const limit = parseInt(req.query.limit) || 10
+            const skip = page * limit;
 
+            const result = await productCollection.find().skip(skip).limit(limit).toArray()
+            res.send(result)
+        })
 
+        // get total proudct count 
+        app.get('/totalproducts_count', async (req, res) => {
+            const result = await productCollection.estimatedDocumentCount()
+            res.send({ totalProductCount: result })
+        })
 
 
 
